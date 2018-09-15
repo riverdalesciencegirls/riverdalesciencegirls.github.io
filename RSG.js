af64a16d4a97c30299d1e2698d7f275bc9af4b91
LaraPalombi <41908179+LaraPalombi@users.noreply.github.com>
2018-09-15 13:40:10 -0400

    Add files via upload

---
+++ RSG.js
@@ -0,0 +1,34 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+
+var scrollEventHandler = function()
+{
+  window.scroll(0, window.pageYOffset)
+}
+
+window.addEventListener("scroll", scrollEventHandler, false);
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
